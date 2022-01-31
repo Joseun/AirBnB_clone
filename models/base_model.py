88b3f4915de2d075f21c7475bc0bedb5c9b3e3b8
Joseun <ologanj@gmail.com>
2022-01-31 02:55:29 +0000

    Added unittests for base_model and trying to fix bug in storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Defining the class BaseModel"""
 from datetime import datetime
 import json
-from models import storage
+# import storage
 import uuid
 
 
@@ -15,43 +15,46 @@ class BaseModel:
             id(:obj:'int'): public instance
         """
 
+        from models import storage
+        tf = '%Y-%m-%dT%H:%M:%S.%f'
         if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                self.__dict__[k] = v
-            self.__dict__["created_at"] = datetime.strptime(value,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
-            self.__dict__["updated_at"] = datetime.strptime(value,
-                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    self.__dict__[k] = datetime.strptime(v, tf)
+                elif k == "updated_at:":
+                    self.__dict__[k] = datetime.strptime(v, tf)
+                else:
+                    self.__dict__[k] = v
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
         storage.new(self)
 
     def to_dict(self):
         """Returns a dictionary containing contents of __dict__ instance"""
 
+        from models import storage
         my_dict = {}
         for k, v in self.__dict__.items():
             my_dict[k] = v
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = my_dict['created_at'].isoformat()
         my_dict['updated_at'] = my_dict['updated_at'].isoformat()
-        print("======={}".format(my_dict))
-        print("##########{}".format(self.__dict__))
+        # print("======={}".format(my_dict))
+        # print("##########{}".format(self.__dict__))
         return my_dict
-
 
     def __str__(self):
         """Returns to standard output the details of the base model"""
 
-        return str("[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__,))
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.to_dict))
 
     def save(self):
         """Updates the public instance attribute """
 
+        from models import storage
         self.updated_at = datetime.now()
-        #storage.new(self)
+        # storage.new(self)
         storage.save()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,8 +2,20 @@
 """Defining the class BaseModel"""
 from datetime import datetime
 import json
+from json import dump, load, dumps
 import uuid
-import models
+from os import path, stat
+from models import base_model, user, place, state, city, amenity, review
+
+BaseModel = base_model.BaseModel
+User = user.User
+Place = place.Place
+State = state.State
+City = city.City
+Amenity = amenity.Amenity
+Review = review.Review
+name_class = ["BaseModel", "City", "State",
+              "Place", "Amenity", "Review", "User"]
 
 
 class FileStorage:
@@ -11,19 +23,6 @@ class FileStorage:
 
     __file_path = "AirBNBfile.json"
     __objects = {}
-
-    def to_dict(self):
-        """Returns a dictionary containing contents of __dict__ instance"""
-
-        my_dict = {}
-        for k, v in self.__dict__.items():
-            my_dict[k] = v
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = my_dict['created_at'].isoformat()
-        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
-        #print("======={}".format(my_dict))
-        #print("##########{}".format(self.__dict__))
-        return my_dict
 
     def all(self):
         """ Method JSON string
@@ -39,66 +38,103 @@ class FileStorage:
         Args:
             - json_string: string to convert to list
         """
-
-        class_id = obj.__class__.__name__ + "." + obj.id
+        print('this is aobj in new oooo  {}'.format(obj))
+        class_name = obj.__class__.__name__
+        id = obj.id
+        class_id = class_name + "." + id
+        # class_id = obj.__class__.__name__ + "." + obj.id
         FileStorage.__objects[class_id] = obj
 
+    def write_json(class_id, new_data):
+        """ Initaliases the FileStorage"""
 
-    @classmethod
-    def save(self):
-        """ Method JSON string
-        Args:
-            - list_dictionaries: list of dicts
-            Returns: JSON representation of the list
-        """
-        list_dicts = []
-        with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-            filecontent = f.read(1)
-        if filecontent:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                list_dicts = json.load(f)
+        if not stat(FileStorage.__file_path).st_size == 0:
+            with open(FileStorage.__file_path, 'r+') as file:
+                # First we load existing data into a dict.
+                file_data = json.load(file)
         else:
-            list_dicts = []
-        print("listdicts is this{}".format(list_dicts))
+            file_data = {}
+        # Join new_data with file_data inside emp_details
+        x = file_data
+        x[class_id] = new_data
+        # Sets file's current position at offset.
+        with open(FileStorage.__file_path, 'r+') as file:
+            # file.seek(0)
+            # convert back to json.
+            json.dump(x, file, indent=4)
+
+    def save(self):
+        """ #Method JSON string
+        #Args:
+         #   - list_dictionaries: list of dicts
+          #  Returns: JSON representation of the list
+        """
+
+       # with open(FileStorage.__file_path, 'r+', encoding='utf-8') as f:
+       # filecontent = f.read()
+       # if filecontent:
+       # with open(FileStorage.__file_path, 'r+', encoding='utf-8') as f:
+       # list_dicts = json.load(f)
+       # else:
+       # list_dicts = []
+       # print("listdicts is this{}".format(list_dicts))
         dict_obj = {}
         print("%%%%%&&&&&#### self.objects {}".format(self.__objects))
         for k, v in self.__objects.items():
-            #print(dir(v))
             dict_obj[k] = v.to_dict()
-            list_dicts.append(dict_obj)
-        print("======")
-        print(dict_obj)
-        print("#########")
-        print(list_dicts)
-        print("=======")
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-            json.dump(list_dicts, f)
+        print("listdicts  *****is this{}".format(dict_obj))
+        # if k == '__class__':
+        #   nam = dict_obj[k]
+        # if k == 'id':
+        # cls_id = dict_obj[k]
+        cls_id = ("{}".format(k))
+        FileStorage.write_json(cls_id, dict_obj)
+        # print(dir(v))
+        # dict_obj[k] = v.to_dict()
+        # list_dicts.append(dict_obj)
+        # print("======")
+        # print(dict_obj)
+        # print("#########")
+        # print(list_dicts)
+        # print("=======")"""
+        # with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+        #    json.dump(FileStorage.__objects, f)
 
     def reload(self):
         """Returns the list of the JSON string representation
         Args:
             - json_string: string to convert to list
         """
-        instance_dict = {}
-        with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-            filecontent = f.read(1)
-            print("filecontent is this {}".format(filecontent))
-            if filecontent == '[':
-                with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                    print("filecontent is this {}".format(filecontent))
-                    list_dicts = json.load(f)
-                    print("list_dicts is this{}".format(list_dicts))
-                    for dicts in list_dicts:
-                        for key, inst_dict in dicts.items():
-                            class_name = key.split('.')[0]
-                            if class_name == 'BaseModel':
-                                for k, v in inst_dict.items():
-                                    instance_dict[k] = v
-                    FileStorage.__objects[class_name] = instance_dict
-                    print("reload output {}".format(FileStorage.__objects))
-                    return FileStorage.__objects
-            else:
-                return instance_dict
-        #except Exception as e:
-            #print(e)
-       # return instance_dict
+        FileStorage.__objects = {}
+        try:
+            if path.isfile(FileStorage.__file_path):
+                if not stat(FileStorage.__file_path).st_size == 0:
+                    with open(FileStorage.__file_path, 'r') as f:
+                        # filecontent = f.read()
+                        # print("filecontent is this {}".format(filecontent))
+                        # if filecontent:
+                        #   with open(FileStorage.__file_path, 'r') as f:
+                        #   print("filecontent is this {}".format(filecontent))
+                        obj_dicts = json.load(f)
+                        #      print("list_dicts is this{}".format(list_dicts))
+                    for key, value in obj_dicts.items():
+                        # for key, inst_dict in dicts.items():
+                        class_name = key.split('.')[0]
+                        if class_name in name_class:
+                            FileStorage.__objects[key] = eval(class_name)(**value)
+                            # for o in obj_dicts.values():
+                            #    cls_name = o["__class__"]
+                            #    del o["__class__"]
+                            #   self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            pass
+        #              for k, v in inst_dict.items():
+        #                 instance_dict[k] = v
+        #  FileStorage.__objects = list_dicts
+        # print("reload output {}".format(FileStorage.__objects))
+        # return FileStorage.__objects
+        # else:
+        #   return instance_dict
+        # except Exception as e:
+        # print(e)
+        # return instance_dict
